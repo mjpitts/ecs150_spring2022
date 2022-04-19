225eb44609dbc6b081764424637b1975bf95764d
Jade Elkins <jadenelkins@gmail.com>
2022-04-18 21:08:29 -0700

    Problem 1 now in a workable state.

--- Lab_1/timeout.c
+++ Lab_1/timeout.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-#include<process.h>
-
+#include<unistd.h>
+#include<signal.h>
 // Use the same delay function from subroutine.c
 void delay(int number_of_seconds)
 {
@@ -18,34 +18,38 @@ void delay(int number_of_seconds)
 
 int main(int argc, char *argv[] ) {
     //catch inccorrect formatting or negative delays
+    printf("%d\n", argc);
     if(argc < 3 || argc > 6){
         printf("Format should be: ./timeout.exe seconds command");
         return 0;
     }
     int timeout = atoi(argv[1]);
-    if( timeout < 0){
+    if(timeout < 0){
         printf("The delay should be positive.");
         return 0;
     }
-    // save cmd/path of cmd to variable as well as sleep duration
-    //testing with subroutine
-    char path[] = "./subroutine.exe";
-    int sleep = atoi(argv[1]);
-    const char * argVec[] = {"p", "arg1", "arg2", "arg3", NULL};
+    char* path = argv[2];
+    int sleepTime = atoi(argv[1]);
+    char const *argVec[] = {"p", "arg1", "arg2", "arg3", NULL};
     const char * envVec[] = {NULL};
 
-    if(argc > 3){
-        for(int i = 3; i < argc; i++){
+    if(argc >= 3){
+        for(int i = 2; i < argc; i++){
             argVec[i-2] = argv[i];
+            printf("%s\n",argVec[i-2]);
         }
     }
 
-    fork();
-    if(execve(path, argVec, envVec) == -1){
-       perror("uh oh what, somthing happened: ");
-    }; 
-
-    
-    
+    pid_t process_id = fork();
+    if(process_id == 0){
+        if(execve(path, argVec, envVec) == -1){
+            perror("uh oh what, somthing happened: ");
+        };
+    }
+    else{
+        sleep(sleepTime);
+        kill(process_id, 9);
+    }
+    printf("Finished executing.\n");
     return 0;
 }
